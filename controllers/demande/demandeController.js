8ab18bead075c426c4a38df2ebfd55ba1a696831
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-09-06 13:13:14 +0300

    demande

--- controllers/demande/demandeController.js
+++ controllers/demande/demandeController.js
@@ -22,50 +22,70 @@ class DemandeController{
         }
     }
 
-    async createDemande(req, res){
+    async createDemande(req, res) {
         try {
-            const correct = req.body.mdp1 === req.body.mdp2;
-            if(correct){
-                const hashed = await bcrypt.hash(req.body.mdp1,10);
-                const dem = await demande.create({
-                    ...req.body,
-                    mdpdemande: hashed
-                });
-                const transporter = nodemailer.createTransport({
-                    service: 'Gmail',
-                    auth: {
-                        user: 'loick6446@gmail.com',
-                        pass: '4743290502Loick'
-                    }
-                });
-                const mailOption = {
-                    from: 'loick6446@gmail.com',
-                    to: req.body.maildemande,
-                    subject: 'Test',
-                    text: `Bonjour,
-                    
-                    Votre demande a bien été envoyé` 
-                };
-                await transporter.sendMail(mailOption);
-                res.status(204).json(dem);
-            }else{
-                res.status(400).send("Mot de passe incorrect !");
-            }
+          const correct = req.body.mdp1 === req.body.mdp2;
+          if (correct) {
+            const hashed = await bcrypt.hash(req.body.mdp1, 10);
+            const dem = await demande.create({
+              ...req.body,
+              mdpdemande: hashed
+            });
+            const transporter = nodemailer.createTransport({
+              service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL_USER,
+                    pass: process.env.APP_PASS,
+                },
+                tls: {
+                  rejectUnauthorized: false
+                }
+            });
+      
+            const mailOption = {
+              from: {
+                name: 'Be mozik',
+                address: process.env.EMAIL_USER
+            },
+              to: req.body.maildemande,
+              subject: 'Test',
+              text: `Bonjour,
+              
+              Votre demande a bien été envoyée
+              ${req.body.prenomdemande}`
+            };
+      
+            await transporter.sendMail(mailOption, (err, info) => {
+              if (err) {
+                console.log(err);
+                return console.log(err);
+              }
+              console.log('Email sent: ' + info.response);
+            });
+      
+            res.status(204).json(dem);
+          } else {
+            res.status(400).send("Mot de passe incorrect !");
+          }
         } catch (error) {
-            res.status(400).send(error);
+          console.log(error);
+          res.status(400).send(error);
         }
-    }
+      }
     
     async deleteDemande(req,res){
         try {
-            const dem = await demande.findByPk(req.params.idDemande);
+            const dem = await demande.findOne({where: {iddemande: req.params.idDemande}});
             if(dem){
-                await demande.destroy(dem);
+                await demande.destroy({
+                    where: { iddemande: req.params.idDemande }
+                });                
                 res.status(204).send();
             }else{
                 res.status(404).send("Demande non trouvée");
             }
         } catch (error) {
+            console.log('erreur '+error);
             res.status(400).send(error);
         }
     }
