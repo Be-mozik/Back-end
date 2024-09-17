43a8b367c800370c454462593b34877aab6026f1
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-09-17 13:57:56 +0300

    base

--- Base.sql
+++ Base.sql
@@ -2,6 +2,7 @@
 -- BASE Mozik
 -- MDP 123
 
+-- Active: 1725437092242@@127.0.0.1@5432@Mozik@public
 create sequence seq_util
 increment by 1
 start with 1;
@@ -14,7 +15,6 @@ create table utilisateur(
     estSuperUtilisateur BOOLEAN DEFAULT false,
     depuisUtilisateur date DEFAULT current_date
 );
-
 insert into utilisateur VALUES(concat('User ',nextval('seq_util')),'Jean','jean@gmail.com','123',true);
 
 create sequence seq_demande
@@ -31,6 +31,40 @@ create table demande(
 
 insert into demande VALUES(concat('Demande ',nextval('seq_demande')),'Yasuo','yasuo@gmail.com','123');
 
+create sequence seq_evenement
+increment by 1
+start with 1;
 
+create table evenement(
+    idEvenement VARCHAR PRIMARY key,
+    idUtilisateur VARCHAR,
+    nomEvenement VARCHAR (255),
+    dateheureEvenement TIMESTAMP,
+    lieuEvenement VARCHAR(255),
+    descriEvenement VARCHAR(255),
+    imgEvenement VARCHAR(255),
+    Foreign Key (idUtilisateur) REFERENCES utilisateur(idUtilisateur)    
+);
 
+create SEQUENCE seq_billet
+INCREMENT by 1
+start with 1;
 
+create table billetEvenement(
+    idEvenement VARCHAR,
+    idBillet VARCHAR PRIMARY key,
+    nomBillet VARCHAR(255),
+    tarifBillet NUMERIC(9,2),
+    Foreign Key (idEvenement) REFERENCES evenement(idEvenement)
+);
+
+create SEQUENCE seq_info
+INCREMENT by 1
+start with 1;
+
+create table infoEvenement(
+    idEvenement VARCHAR,
+    idInfo VARCHAR PRIMARY KEY,
+    numeroInfo VARCHAR,
+    Foreign Key (idEvenement) REFERENCES evenement(idEvenement)
+);
