d84d3c7866ee7f3f0630f7139e68011e5cbb8b23
Rakotoarivo Loick <120171050+Avocadorable0@users.noreply.github.com>
2024-11-18 13:28:07 +0300

    base

--- config/db.js
+++ config/db.js
@@ -1,10 +1,19 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const seque = new Sequelize(process.env.DB, {
-    protocol: 'postgres',
-    dialect: 'postgres',
-});
+// const seque = new Sequelize(process.env.DB, {
+//     protocol: 'postgres',
+//     dialect: 'postgres',
+// });
+
+const seque = new Sequelize(process.env.DB_NAME,
+                                 process.env.DB_USER,
+                                 process.env.DB_PASS,
+                                  { 
+                                    host: process.env.DB_HOST,
+                                    dialect: 'postgres',
+                                     protocol: 'postgres'
+                                 });
 
 const connect = async () => {
     try {
